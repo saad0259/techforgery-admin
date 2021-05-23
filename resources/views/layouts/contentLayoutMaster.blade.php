aa78859bd45ba18befb70f610f78bee4e81db0b6
Rehman Ali <53118785+rehmanaliawans@users.noreply.github.com>
2021-05-23 13:42:35 +0500

    horizontal scroll bar add

--- resources/views/layouts/contentLayoutMaster.blade.php
+++ resources/views/layouts/contentLayoutMaster.blade.php
@@ -28,12 +28,13 @@ License: You must have a valid license purchased only from themeforest(the above
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui,shrink-to-fit=no">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>@yield('title') | Materialize - Material Design Admin Template</title>
   <link rel="apple-touch-icon" href="{{asset('images/favicon/apple-touch-icon-152x152.png')}}">
   <link rel="shortcut icon" type="image/x-icon" href="{{asset('images/favicon/favicon-32x32.png')}}">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
   {{-- Include core + vendor Styles --}}
   @include('panels.styles')
